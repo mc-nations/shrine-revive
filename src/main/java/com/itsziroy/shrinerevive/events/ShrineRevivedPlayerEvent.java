aa98b8f43f8529d42e0ef7e455f188d339692dcb
Yannik Hahn <yannik@h4hn.de>
2023-10-09 22:10:39 +0200

    ShrineRevivedPlayerEvent propagates McUser

--- src/main/java/com/itsziroy/shrinerevive/events/ShrineRevivedPlayerEvent.java
+++ src/main/java/com/itsziroy/shrinerevive/events/ShrineRevivedPlayerEvent.java
@@ -2,15 +2,23 @@ package com.itsziroy.shrinerevive.events;
 
 import com.itsziroy.bukkitredis.events.ExtensibleEvent;
 import com.itsziroy.bukkitredis.events.SimplePlayer;
+import com.itsziroy.bukkitredis.events.SimplePlayer;
 
 public class ShrineRevivedPlayerEvent extends ExtensibleEvent {
 
     public static String NAME = "shrine_revived_player";
+
+    private final SimplePlayer simplePlayer;
     public ShrineRevivedPlayerEvent(String uuid, String name) {
         super(NAME);
+        this.simplePlayer = new SimplePlayer(uuid, name);
 
-        this.put("minecraft_user", new SimplePlayer(uuid, name));
+        this.put("minecraft_user", simplePlayer);
 
         this.executeCallbacks();
+    }
+
+    public SimplePlayer getSimplePlayer() {
+        return simplePlayer;
     }
 }
